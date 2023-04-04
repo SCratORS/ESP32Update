28c539db6f56f6469f765ac01f8b6dffca3c6633
SCratORS <scrators@mail.ru>
2023-04-04 16:39:00 +0300

    Update esp_rom_md5.h

--- src/esp_rom_md5.h
+++ src/esp_rom_md5.h
@@ -28,13 +28,13 @@ extern "C" {
  * @brief Type defined for MD5 context
  *
  */
- /**   
+ 
 typedef struct MD5Context {
     uint32_t buf[4];
     uint32_t bits[2];
     uint8_t in[64];
 } md5_context_t;
-*/
+
 #define ESP_ROM_MD5_DIGEST_LEN 16
 
 /**
